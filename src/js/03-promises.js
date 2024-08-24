6eae701f82762357d22fe3433c322f3e9f386a77
Alina Dogaru <alina.elena_dogaru@yahoo.com>
2024-08-24 19:20:54 +0300

    First commit

---
+++ src/js/03-promises.js
@@ -0,0 +1,11 @@
+
+"use strict";
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
+  }
+}
